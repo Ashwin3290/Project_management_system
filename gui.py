7129e4dc44d15440ef86b86d38a699592d27e546
Ashwin Choubey <ashwinchoubey03092002@gmail.com>
2022-04-16 18:49:31 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,10 +4,10 @@ class login():
     def call(frame,user_var,passw_var):
         name=user_var.get()
         password=passw_var.get()
-        x=["Ash","Admin"]
-        y=["hi","master"]
+        x=["Ash",""]
+        y=["hi",""]
         if name in x and password in y and x.index(name)==y.index(password):
-            if name=="Admin":
+            if name=="":
                 frame.destroy()
                 next=selection_admin()
                 next.screen()
@@ -62,34 +62,67 @@ class selection_admin:
         next.screen()
         
     
-    def subject(frame,x):
+    def subject_add(frame,x):
         frame.destroy()
         frame=Frame(root,height=400,width=600)
         frame.grid(row=0, column=0, sticky='nsew')
-        ds=Button(frame,text="Data science")
-        os=Button(frame,text="Operating system")
-        toc=Button(frame,text="Theory of computation")
-        cp=Button(frame,text="Computer Programming II")
-        ds.place(relx=.5, rely=.5,anchor= CENTER,y=-40,command=lambda:eval("selection_admin."+x+"(frame)"))
-        os.place(relx=.5, rely=.5,anchor= CENTER,y=-10,command=lambda:eval("selection_admin."+x+"(frame)"))
-        toc.place(relx=.5, rely=.5,anchor= CENTER,y=10,command=lambda:eval("selection_admin."+x+"(frame)"))
-        cp.place(relx=.5, rely=.5,anchor= CENTER,y=40,command=lambda:eval("selection_admin."+x+"(frame)"))
+        ds=Button(frame,text="Data science",command=lambda:selection_admin.add_display(frame,'os'))
+        os=Button(frame,text="Operating system",command=lambda:selection_admin.add_display(frame,'ds'))
+        toc=Button(frame,text="Theory of computation",command=lambda:selection_admin.add_display(frame,'toc'))
+        cp=Button(frame,text="Computer Programming II",command=lambda:selection_admin.add_display(frame,'cp'))
+        ds.place(relx=.5, rely=.5,anchor= CENTER,y=-60)
+        os.place(relx=.5, rely=.5,anchor= CENTER,y=-20)
+        toc.place(relx=.5, rely=.5,anchor= CENTER,y=20)
+        cp.place(relx=.5, rely=.5,anchor= CENTER,y=60)
 
-    def remove(frame):
-        label=Label(frame,text="Enter assignment number")
+    def subject_remove(frame,x):
+        frame.destroy()
+        frame=Frame(root,height=400,width=600)
+        frame.grid(row=0, column=0, sticky='nsew')
+        print("selection_admin."+x+"(frame,'ds')")
+        ds=Button(frame,text="Data science",command=lambda:selection_admin.remove_display(frame,'os'))
+        os=Button(frame,text="Operating system",command=lambda:selection_admin.remove_display(frame,'ds'))
+        toc=Button(frame,text="Theory of computation",command=lambda:selection_admin.remove_display(frame,'toc'))
+        cp=Button(frame,text="Computer Programming II",command=lambda:selection_admin.remove_display(frame,'cp'))
+        ds.place(relx=.5, rely=.5,anchor= CENTER,y=-60)
+        os.place(relx=.5, rely=.5,anchor= CENTER,y=-20)
+        toc.place(relx=.5, rely=.5,anchor= CENTER,y=20)
+        cp.place(relx=.5, rely=.5,anchor= CENTER,y=60)
+
+    def remove_display(frame,sub):
+        frame.destroy()
+        frame=Frame(root,height=400,width=600)
+        frame.grid(row=0, column=0, sticky='nsew')
+        label=Label(frame,text="Assignment number")
         num_var=StringVar()
-        entry=Entry(frame,num_var)
+        entry=Entry(frame,textvariable=num_var)
         remove=Button(frame,text="Remove")
-        label.place(relx=.5,rely=.5,anchor= CENTER,y=-10)
-        remove.place(relx=.5,rely=.5,anchor= CENTER,y=10)
+        label.place(relx=.5,rely=.5,anchor=CENTER,x=-100,y=0)
+        entry.place(relx=.5,rely=.5,anchor=CENTER,x=25,y=0)
+        remove.place(relx=.5,rely=.5,anchor=CENTER,y=50)
+
         
-    def add(frame):
-        
-        entry1=Entry(frame,)
-        entry2=Entry(frame,)
-        entry3=Entry(frame,)
-        entry4=Entry(frame,)
-        entry5=Entry(frame,)
+    def add_display(frame,sub):
+        frame.destroy()
+        frame=Frame(root,height=400,width=600)
+        frame.grid(row=0, column=0, sticky='nsew')
+        anum=StringVar()
+        wl=StringVar()
+        dd=StringVar()
+        Label1=Label(frame,text="Assignment num")
+        Label2=Label(frame,text="Work link")
+        Label3=Label(frame,text="Due date")
+        entry1=Entry(frame,textvariable=anum)
+        entry2=Entry(frame,textvariable=wl)
+        entry3=Entry(frame,textvariable=dd)
+        submit=Button(frame,text="submit")#command=lambda:pdm.add())
+        Label1.place(relx=.5,rely=.5,anchor=CENTER,x=-100,y=-40)
+        Label2.place(relx=.5,rely=.5,anchor=CENTER,x=-80,y=0)
+        Label3.place(relx=.5,rely=.5,anchor=CENTER,x=-80,y=40)
+        entry1.place(relx=.5,rely=.5,anchor=CENTER,x=25,y=-40)
+        entry2.place(relx=.5,rely=.5,anchor=CENTER,x=25,y=0)
+        entry3.place(relx=.5,rely=.5,anchor=CENTER,x=25,y=40)
+        submit.place(relx=.5,rely=.5,anchor=CENTER,y=100)
 
 
     
@@ -97,8 +130,8 @@ class selection_admin:
         super()
         frame=Frame(root,height=400,width=600)
         frame.grid(row=0, column=0, sticky='nsew')
-        add=Button(frame,text="Add assignment",command=lambda:selection_admin.subject(frame))
-        remove=Button(frame,text="Remove assignment",command=lambda:selection_admin.subject(frame))
+        add=Button(frame,text="Add assignment",command=lambda:selection_admin.subject_add(frame,"add_display"))
+        remove=Button(frame,text="Remove assignment",command=lambda:selection_admin.subject_remove(frame,"remove_display"))
         pending=Button(frame,text="Show all pending assignments",command=lambda:selection_admin.subject(frame))
         mark=Button(frame,text="Mark complete",command=lambda:selection_admin.subject(frame))
         logout=Button(frame,text="logout",command=lambda:selection_admin.call(frame))
